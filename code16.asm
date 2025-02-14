8615ccf845c2e5b157c02bdffa4aef0c73fa72b0
.asm <irajmirzazadeh2024@gmail.com>
2025-02-14 06:27:03 -0800

    Update code16.asm

--- code16.asm
+++ code16.asm
@@ -1,13 +1,13 @@
 section .data
     msg db "Hello", 0xA  ; "Hello" followed by a newline (ASCII 10)
     len equ $ - msg      ; Length of the message
-    num db 5             ; Counter initialized to 5
+    i db 5             ; Counter initialized to 5
 
 section .text
     global _start
 
 _start:
-
+    ;mov rcx,5
 loop_start:
     ; Write syscall: write(1, msg, len)
     mov rax, 1           ; syscall number for sys_write
@@ -16,9 +16,13 @@ loop_start:
     mov rdx, len         ; message length
     syscall              ; invoke syscall
 
-    dec byte [num]       ; Decrement counter (treating num as a byte)
-    cmp byte [num], 0    ; Compare num with 0
+    ;dec rcx
+
+    dec byte [i]       ; Decrement counter (treating num as a byte)
+    cmp byte [i], 0    ; Compare num with 0
     jg loop_start        ; Jump back if num is greater than zero
+    ; jnz loop_start
+    ; loop loop_start     ;rcx-1<>0 --> loop_start
 
     ; Exit syscall: exit(0)
     mov rax, 60          ; syscall number for sys_exit
