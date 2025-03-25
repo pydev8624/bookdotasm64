803cbe22249d56a14b1ccafe9c71a3ed7c2f7a37
.asm <irajmirzazadeh2024@gmail.com>
2025-03-25 20:02:04 +0330

    Update code32.asm

--- code32.asm
+++ code32.asm
@@ -1,28 +1,27 @@
 section .data
-    message db "Hello", 10
-    message_len equ $ - message
+    msg db "hello", 0xA
+    len equ 6
 
 section .text
     global _start
-    
-    ; Function prototype: void hello()
-hello:
-    push rbp            ; Save old base pointer
-    mov rbp, rsp        ; Set up new base pointer
 
-    mov rax, 1          ; syscall: write
-    mov rdi, 1          ; file descriptor: stdout
-    mov rsi, message    ; message to write
-    mov rdx, message_len ; message length
-    syscall             ; invoke syscall
-    
-    pop rbp             ; Restore old base pointer
-    ret                 ; Return to caller
+hello:
+    push rbp
+    mov rbp, rsp
+    ;sub rsp,8  ; stack aligment : cpu management
+
+    mov rax,1
+    mov rdi,1
+    mov rsi,msg
+    mov rdx,len
+    syscall
+
+    pop rbp
+    ret
 
 _start:
-    call hello          ; Call hello function
+    call hello
 
-    mov rax, 60         ; syscall: exit
-    xor rdi, rdi        ; status: 0
-    syscall             ; invoke syscall
-
+    mov rax,60
+    xor rdi,rdi
+    syscall
